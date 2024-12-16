e20384d89957758f1479757902661d4736e7e3c8
yasnagat <yasmim.nagat@gmail.com>
2024-12-16 19:07:37 -0300

    atualização do tradutor de sinopses.
    Signed-off-by: yasnagat <yasmim.nagat@gmail.com>

--- src/main/java/br/com/alura/screenmatch/service/translator/MyMemoryRequest.java
+++ src/main/java/br/com/alura/screenmatch/service/translator/MyMemoryRequest.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.URLEncoder;
 
+// cria uma chamada para API MyMemory para traduzir o texto da sinopse
 public class MyMemoryRequest {
     public static String translate(String text) {
+        // criando os objetos das classes de serviço
         ObjectMapper mapper = new ObjectMapper();
         APIConsumer consumer = new APIConsumer();
 
@@ -16,8 +18,10 @@ public class MyMemoryRequest {
 
         String URL = "https://api.mymemory.translated.net/get?q=" + texts + "&langpair=" + langpair;
 
+        // request para API
         String json = consumer.dataObtainer(URL);
 
+        // executa a traducao
         TranslatedData translation;
         try {
             translation = mapper.readValue(json, TranslatedData.class);
